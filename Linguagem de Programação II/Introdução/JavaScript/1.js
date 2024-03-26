d48c4edbb8c6e92fe82fc2cf7feff23c50672c42
MatheusMDev <matheu.miranda@hotmail.com>
2024-03-26 16:54:07 -0300

    aula de lp

---
+++ Linguagem de Programação II/Introdução/JavaScript/1.js
@@ -0,0 +1,12 @@
+<script>
+    {/* Declaração de variaveis */}
+    var numero1 = 5;
+    var numero2 = 10;
+
+    {/* Calculo */}
+    var Resultado = numero1 + numero2;
+
+    {/* Saída */}
+    console.log("O resultado é: "+ Resultado);
+    alert("O Resultado é: " +Resultado);
+</script>
